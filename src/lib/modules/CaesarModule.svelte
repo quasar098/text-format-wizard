f7d5ca675435930c144200e481c9204616de2c6f
quasar098 <patellmlg@gmail.com>
2023-03-09 13:40:13 -0500

    more stuff

---
+++ src/lib/modules/CaesarModule.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { get } from 'svelte/store'
+    import { ModuleType } from "../ts/types";
+    import { recipeModules } from '../ts/stores';
+    import InputBox from '../InputBox.svelte';
+
+    export let info = {};
+</script>
+
+<p>Shift amount</p>
+<InputBox bind:value={info.shift}/>
+
+<style>
+
+</style>
---
+++ src/lib/modules/ReflectModule.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { get } from 'svelte/store'
+    import { ModuleType } from "../ts/types";
+    import { recipeModules } from '../ts/stores';
+    import InputBox from '../InputBox.svelte';
+
+    export let info = {};
+</script>
+
+<p>No arguments here</p>
+
+<style>
+
+</style>
